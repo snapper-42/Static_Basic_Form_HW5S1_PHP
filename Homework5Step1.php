56b88f07c792f67a0a130eb5366c2caeeaaef777
Brandon Roberts <broberts20281@cincinnatistate.edu>
2014-12-02 20:18:57 -0500

    Basic static page

---
+++ Homework5Step1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/htm14/strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+    <head runat="server">
+
+        <title></title>
+
+    </head>
+
+    <body>
+
+        Name: Brandon Roberts <br />
+        Class: PHP <br />
+        Abstract: Homework 5 - Step 1 - Static Text <br />
+        <br />
+        <br />
+        Static Text: 1 I Love Star Trek<br />
+        <br />
+        <form id="form1" runat="server">
+        <div>
+    
+        </div>
+        </form>
+
+    </body>
+
+</html>
